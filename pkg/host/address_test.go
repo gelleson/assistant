fd28643a196afb0685f9529ad0a65a17dd85218d
Altynbek <go.gelleson@gmail.com>
2020-10-03 17:03:34 +0600

    add test

--- pkg/host/address_test.go
+++ pkg/host/address_test.go
@@ -25,23 +25,25 @@
 package host
 
 import (
-	"net"
 	"strings"
+	"testing"
 )
 
-// GetIPAddress provide local ip address of the server in local network
-func GetIPAddress() string {
-	addressArr, err := net.InterfaceAddrs()
-	if err != nil {
-		return ""
+func TestGetIPAddress(t *testing.T) {
+	tests := []struct {
+		name      string
+		wantStart string
+	}{
+		{
+			name:      "test_local_address",
+			wantStart: "192",
+		},
 	}
-	for _, address := range addressArr {
-		// check the address type and if it is not a loopback the display it
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil && strings.Contains(ipnet.IP.String(), "192") {
-				return ipnet.IP.String()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetIPAddress(); !strings.Contains(got, tt.wantStart) {
+				t.Errorf("GetIPAddress() = %v, want %v", got, tt.wantStart)
 			}
-		}
+		})
 	}
-	return ""
 }
