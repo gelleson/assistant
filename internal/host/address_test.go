d7428b0df6e2a0e9cb54b70da3b374d7331a0333
Altynbek <go.gelleson@gmail.com>
2020-10-03 18:16:09 +0600

    fix test

--- internal/host/address_test.go
+++ internal/host/address_test.go
@@ -41,7 +41,7 @@ func TestGetIPAddress(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetIPAddress(); !strings.Contains(got, tt.wantStart) {
+			if got := GetIPAddress(); !strings.Contains(got, tt.wantStart) && strings.Contains(got, "127") {
 				t.Errorf("GetIPAddress() = %v, want %v", got, tt.wantStart)
 			}
 		})
